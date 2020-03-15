b898aeb6467e899f24d170c7e799474bd149ccd9
Lucas Fioletti-Nieder <lulu57580@gmail.com>
2020-03-15 09:51:44 +0100

    màj des fichiers

--- Interface/Connexe/bouton-ajout.php
+++ Interface/Connexe/bouton-ajout.php
@@ -1,43 +1,17 @@
 <html>
 <head>
-<title>Bouton ajout</title>
-<style>
-	background-color: whitesmoke;
-	img {
-  		width: inherit;
-  		height: inherit;
-  		border-radius: inherit;
-	}
-
-/* Bonus : keep aspect-ratio */ 
-	img {
-  		object-fit: cover;
-  		object-position: right center;
-	}
-
-/* Bonus : effect on :hover */
-	img {
-  		transition: object-position .25s;
-	}
-	img:hover {
-  		object-position: center;
-	}
-	.bouton{
-		width: 100%;
-		text-align: center;
-		height: 100%
-	}
-	.bouton_img{
-		top: 50%;
-		transform: translateY(-50%);
-		position: relative;
-		height: 200px;
-	}
-</style>
+	<title>Bouton ajout</title>
+	<link rel="stylesheet" href="../style.css" type="text/css" />
+	<style type="text/css">
+		body{
+			height: 350px;
+			width: 350px;
+			margin-left: auto;
+			margin-right: auto;
+		}
+	</style>
 </head>
 <body>
-<div class="bouton">
 <img src="../Image/Ajoute.PNG" alt="bouton ajout" class="bouton_img">
-</div>
 </body>
 </html>
--- Interface/Connexe/connexion.php
+++ Interface/Connexe/connexion.php
@@ -5,34 +5,45 @@
 		<style type="text/css">
 		.foc{
 			/*Défini les boutons oranges*/
- 		background-color:#FF8000;
+ 		background-color:#EB681F;
+ 		opacity: 0.7;
  		border: none;
   		color: white;
   		padding: 16px 60px;
-  		text-decoration: none;
-  		margin: 4px 2px;
- 		cursor: pointer;
- 		border-radius: 32px;
- 		width: 200px
-
-		}
+ 		border-radius: 5em;
+ 		width: 15em;
+ 		margin-top: 0.5em;
+ 		text-align: center;
+ 		}
 		div{
-	position: relative;
-	margin-left: auto;
-	margin-right: auto;
-	width: 25%;
-}
+			margin-left: auto;
+			margin-right: auto;
+			margin-top: 7em;
+			width: 15em;
+		}
+		p{
+			background-color: #f64545;
+			border-radius: 5em;
+			padding: 0.5em;
+			text-align: center;
+			color: white;
+		}
 		</style>
 	</head>
-		<img id="logo" src="../Image/spaLogo.png">
+		<a href="../home.php"><img id="logo" src="../Image/spaLogo.png"></a>
 	<body>
-		<form action="interface-benevole.php" method="get" autocomplete="on">
+		<form action="../PHP/phpCo.php" method="get" autocomplete="on">
 		<div>
-			<input class="foc" type="text" name="id"value=""  placeholder="Identifiant"/>
+			<center><input class="foc" type="text" name="id"value=""  placeholder="Identifiant"/></center>
 			<br>
-			<input class="foc" type="password" name="mdp"value="" placeholder="Mot de passe"/>
+			<center><input class="foc" type="password" name="mdp"value="" placeholder="Mot de passe"/></center>
 			<br>
-			<input class="fb" type="submit"value="Valider"/>
+			<center><button class="fb" name="valider">Valider</button></center>
+			<?php
+			if (!empty($_GET['msg'])) {
+				echo "<p>".$_GET['msg']."</p>";
+			}
+			?>
 		</div>
 		</form>
 	</body>
