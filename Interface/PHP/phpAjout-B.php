e3b4a3ebbf936020b8abbee8109842fce38087d3
Tiphaine Porte <tiphaine.porte06@gmail.com>
2020-03-18 10:58:29 +0100

    push

--- Interface/PHP/phpAjout-B.php
+++ Interface/PHP/phpAjout-B.php
@@ -1,4 +1,23 @@
-<<<<<<< HEAD
+<?php session_start()?>
+<html>
+<head>
+	<title>ajoutBénévole</title>
+</head>
+<style type="text/css">
+	html{
+		background-color: pink;
+	}
+</style>
+<body>
+	<?php
+		if(isset($_GET["valider"])){
+			include "../bd.php";
+			$bdd = getBD(kitie2);
+		}
+
+	?>	
+</body>
+
 <?php session_start()?>
 <html>
 <head>
@@ -18,25 +37,4 @@
 
 	?>	
 </body>
-=======
-<?php session_start()?>
-<html>
-<head>
-	<title>php</title>
-</head>
-<style type="text/css">
-	html{
-		background-color: pink;
-	}
-</style>
-<body>
-	<?php
-		if(isset($_GET["valider"])){
-			include "../bd.php";
-			$bdd = getBD(kitie2);
-		}
-
-	?>	
-</body>
->>>>>>> 698466118abc3a0a512215be0ae628dddc101d20
 </html>
