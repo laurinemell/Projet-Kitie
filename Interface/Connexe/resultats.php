d2bf11cb751c4122cb64a8b708f8a4cae51d6c14
Lucas Fioletti-Nieder <lulu57580@gmail.com>
2020-03-26 16:36:31 +0100

    Mise à jour et selection

--- Interface/Connexe/resultats.php
+++ Interface/Connexe/resultats.php
@@ -12,44 +12,43 @@
 	include "../bd.php";
 	$bdd = getBD();
 	$requete="select * from chien, etrerace, etredecouleur where";
-	if(isset($_GET["Race"])){
+	//
+	//Etat de Race
+	if (isset($_GET["Race"])) {
 		$race=' chien.idChien=etrerace.idChien and etrerace.idRace='.$_GET["Race"];
-		/*if($_GET["Sexe"]="" || $_GET["Couleur"]=""){
-			$bis=" and ";
-			$race=$race.$bis;
-		}
 		$requete=$requete.$race;
-	/*}
-	if(isset($_GET["Couleur"])){
-		$couleur=' etredecouleur.idChien=chien.idChien and etredecouleur.idCouleur='.$_GET["Couleur"];
-		if(isset($_GET["Sexe"])){
-			$sexe=" and ";
-			$couleur=$couleur.$sexe;
+		if(!empty($_GET["Sexe"]) or !empty($_GET["Couleur"])){
+			$requete=$requete." and";
 		}
-		$requete=$requete.$couleur;
 	}
+	//
+	//Etat de Couleur
+	if (isset($_GET["Couleur"])) {
+		$couleur=' etredecouleur.idChien=chien.idChien and etredecouleur.idCouleur='.$_GET["Couleur"];
+		$requete=$requete.$couleur;
+		if(empty($_GET["Sexe"])){
+			$requete=$requete." and";
+		}
+	}
+	//
+	//Etat du Sexe
 	if(isset($_GET["Sexe"])){
-		$sexe="chien.idSexe=".$_GET["Sexe"];
+		$sexe=" chien.idSexe=".$_GET["Sexe"];
 		$requete=$requete.$sexe;
 	}
+	//Requête entrée
 	$rep = $bdd->query($requete);
 	if(empty($rep)){
 		echo '<p>Désolé aucuns résultats</p>';
-	}*/
-
-	//Début de l'afichage
-	echo "Requête effectuée";
-	//$sexe = $bdd->query('select sexe.NomSexe from chien,sexe where chien.idChien='.$_GET["identifiant"].' and chien.idSexe=sexe.IdSexe');
-	//$race = $bdd->query('select races.nomRace from races, etrerace where etrerace.idChien='.$_GET["identifiant"].' and races.idRace=etrerace.idRace ');
+		echo "<meta http-equiv='refresh' content='1; URL=criteres.php?msg='Aucun résultats désolé'";
+	}
 	while ($ligne = $rep ->fetch()) {
 		echo '<div class="chiens">';
 			echo '<a href="ficheChien.php?identifiant='.$ligne["idChien"].'"><img class="rond" src="../../BD/photo/'.$ligne["photo"].'"/></a>';
 			echo '<p class="prenom">Bonjour, je suis : '.$ligne["nomChien"].'</p>';
-			echo '<p class="nais"> Né.e : '.$ligne["dateNaissance"].'</p>';
-			/*echo '<p class="sexe">'.$ligne["NomSexe"].'</p>';*/
 			echo '<p class="ste">'.$ligne["idSterilisation"].'</p>';
 		echo '</div>';
-			} 
+	}
 	?>
 	<hr class="separation"/>
 </body>
