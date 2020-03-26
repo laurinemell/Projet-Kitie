c060f2b4399f774b39470fe27fb7979bc5a47c8a
Laurine Mellado <laurinemellado@gmail.com>
2020-03-26 13:56:09 +0100

    modif pour amélia

--- Interface/Connexe/enregistrement_chien_dans_la_BD.php
+++ Interface/Connexe/enregistrement_chien_dans_la_BD.php
@@ -6,44 +6,46 @@
 <?php
 
 function enregistrer ($identifiant, $p, $date, $dateentree, $sexe, $sterilisation, $tarif, $mordeur, $box, $couleur, $lof, $vaccin, $datevaccin, $etat, $description) {
-include "bd.php";
-$bdd = getBD();
- $req=("INSERT INTO chien (idChien,nomChien, dateNaissance, dateEntree, idSexe, idSterilisation, idTarification, idMordeur, description) VALUES ("."'".$_GET['identifiant']."'".","."'".$_GET['p']."'".", "."'".$_GET['date']."'".", "."'".$_GET['dateentree']."'".", ".$_GET['sexe'].", "."'".$_GET['sterilisation']."'".", "."'".$_GET['tarif']."'".","."'".$_GET['mordeur']."'".","."'".$_GET['description']."'".")");
-$i=0
- while ($i<count($couleur) ) {
+	include "bd.php";
+	$bdd = getBD();
+ 	$req=("INSERT INTO chien (idChien,nomChien, dateNaissance, dateEntree, idSexe, idSterilisation, idTarification, idMordeur, description) VALUES ("."'".$_GET['identifiant']."'".","."'".$_GET['p']."'".", "."'".$_GET['date']."'".", "."'".$_GET['dateentree']."'".", ".$_GET['sexe'].", "."'".$_GET['sterilisation']."'".", "."'".$_GET['tarif']."'".","."'".$_GET['mordeur']."'".","."'".$_GET['description']."'".")");
+	$i=0;
+	
+ 	while ($i<count($couleur) ) {
 
- $req1= ("INSERT INTO etredecouleur (idChien,idCouleur) VALUES ("."'".$_GET['identifiant']."'".","."'".$_GET['couleur']."'".")");
- echo $req1;
-$i++;
-} 
+ 		$req1="INSERT INTO etredecouleur (idChien,idCouleur) VALUES ('".$identifiant."','".$couleur[$i]."')";
+ 		echo $req1;
+ 		$rep1=$bdd->query($req1);
+		$i++;
+	} 
 
-  $req2= ("INSERT INTO loger (idBox,idChien) VALUES ("."'".$_GET['box']."'".","."'".$_GET['identifiant']."'".")");
+  	$req2= "INSERT INTO loger (idBox,idChien) VALUES ('".$_GET['box']."','".$_GET['identifiant']."')";
 
- $req3=("INSERT INTO etrevaccine (idChien,idVaccin,dateVaccin) VALUES ("."'".$_GET['identifiant']."'".","."'".$_GET['vaccin']."'".","."'".$_GET['datevaccin']."'".")");
+ 	$req3="INSERT INTO etrevaccine (idChien,idVaccin,dateVaccin) VALUES ('".$_GET['identifiant']."','".$_GET['vaccin']."','".$_GET['datevaccin']."')";
 
- $req4=("INSERT INTO etrerace (idChien,idRace,idCategorie,idLof) VALUES ("."'".$_GET['identifiant']."'".","."'".$_GET['race']."'".","."'".$_GET['etat']."'".","."'".$_GET['lof']."'".")");
+ 	$req4="INSERT INTO etrerace (idChien,idRace,idCategorie,idLof) VALUES ('".$_GET['identifiant']."','".$_GET['race']."','".$_GET['etat']."','".$_GET['lof']."')";
 
-echo $req;
-echo $req1;
-echo $req2;
-echo $req3;
-echo $req4;
+	echo $req;
+	echo $req1;
+	echo $req2;
+	echo $req3;
+	echo $req4;
 
-$reponse = $bdd->query($req);
-$reponse->closeCursor();
+	$reponse = $bdd->query($req);
+	$reponse->closeCursor();
 
-$reponse1 = $bdd->query($req1);
-$reponse1->closeCursor();
+	$reponse1 = $bdd->query($req1);
+	$reponse1->closeCursor();
 
-$reponse2 = $bdd->query($req2);
-$reponse2->closeCursor();
+	$reponse2 = $bdd->query($req2);
+	$reponse2->closeCursor();
 
-$reponse3 = $bdd->query($req3);
-$reponse3->closeCursor();
+	$reponse3 = $bdd->query($req3);
+	$reponse3->closeCursor();
 
-$reponse4 = $bdd->query($req4);
-$reponse4->closeCursor();
- }
+	$reponse4 = $bdd->query($req4);
+	$reponse4->closeCursor();
+}
 
 if($_GET['identifiant']==""){
 
