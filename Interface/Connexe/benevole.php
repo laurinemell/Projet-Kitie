80b094144993e7bae4f801bacef4df777250cf73
Lucas Fioletti-Nieder <lulu57580@gmail.com>
2020-03-31 19:35:35 +0200

    Connexion aboutie ainsi que la procédure de connexion, mise à jours de la page bénévole en cours de dévenollement

--- Interface/Connexe/benevole.php
+++ Interface/Connexe/benevole.php
@@ -1,9 +1,12 @@
+<?php session_start() ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Benevole</title>
 	<link rel="stylesheet" href="../Style/style.css" type="text/css" />
 	<link rel="stylesheet" href="../Style/benevole.css" type="text/css" />
+	<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+	<script type="text/javascript" src="../JavaScript/graphe.js"></script>
 </head>
 <body>
 	<a href="../home.php"><img id="logo" src="../Image/spaLogo.png"></a>
@@ -13,11 +16,26 @@
 	</div>
 	<div id="donnee">
 		<div id="selection">
+			<ul>
+				<?php
+				include "../bd.php";
+				$bdd = getBD();
+				$sex= $bdd->query('SELECT COUNT(chien.idChien) as nombre, sexe.NomSexe as sexe FROM chien,sexe where chien.idSexe=sexe.IdSexe GROUP BY chien.idSexe');
+				while ($ligne = $sex ->fetch()) {
+
+				}
+				?>
+				<input type="checkbox" onclick="graphe(1)">Sexe<br>
+				<input type="checkbox" onclick="graphe(2)">Race<br>
+				<input type="checkbox" onclick="graphe(3)">Couleur<br>
+				<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+			</ul>
 		</div>
 	</div>
 	<div id="apercu">
 		<input type="search" id="barreRecherche" name="name" placeholder="Recherche par identifiant ou nom">
 		<?php
+		//echo '<p> Bonjour, '.$_SESSION["prenom"].'.</p>'
 		include "../bd.php";
 		$bdd = getBD();
 		$rep = $bdd->query('SELECT * FROM chien ORDER BY dateEntree DESC ');
@@ -28,6 +46,7 @@
 		}
 		?>
 	</div>
+	<a href="../PHP/sessionDestruction.php" target="_blank"> <input id="ab" class="fo" type="button" type="button" value="Deconnexion"></a>
 	<center><a href="../../BD/bd.php" target="_blank"> <input id="ic" class="fo" type="button" value="Information sur les chiens"> </a></body></center>
 </body>
 </html>
--- Interface/PHP/phpCo.php
+++ Interface/PHP/phpCo.php
@@ -1,39 +1,59 @@
 <?php session_start()?>
 <html>
 <head>
-	<title>php</title>
+	<title>Connexion</title>
+	<link rel="stylesheet" href="../Style/style.css" type="text/css" />
 </head>
 <style type="text/css">
-	html{
-		background-color: red;
-	}
+	body {
+  background: linear-gradient(45deg, #3773C7, #EB681F);
+  background-size: 4000% 4000%;
+  animation: gradient 2s ease infinite; /*Je l'ai mis plus lent c'est plus joli*/
+  margin-left :auto;
+  margin-right : auto;
+}
+
+@keyframes gradient {
+    0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
+}
+h1{
+	margin-top: 5em;
+	margin-left: 5em;
+	font-size: 3em;
+	color: white;
+}
 </style>
 <body>
+	<h1>Patientez . . .</h1>
 	<?php
-	$nom = $_GET["id"];
-	$mdp = $_GET["mdp"];
 	if(isset($_GET["valider"])){
 		include "../bd.php";
 		$bdd = getBD();
 		$rep = $bdd->query('select * from utilisateur where mdp="'.$_GET["mdp"].'" and mail="'.$_GET["id"].'"');
-		$data = $rep ->fetch();
-		echo "Session créée";
-			if(!empty($data)){
-				$_SESSION["id"] = $data["idUtilisateur"];
-				$_SESSION["nom"] = $data["nom"];
-				$_SESSION["prenom"] = $data["prenom"];
-				$_SESSION["Statu"] = $data["idStatu"];
-				echo "<meta http-equiv='refresh' content='1; URL=../home.php?id=".$_GET['id']."'>";
-			}
-			else{
-				echo "<meta http-equiv='refresh' content='1; URL=../Connexe/connexion.php?msg='Attention, identifiant incorrects '>";
-			}
+		$ligne = $rep ->fetch();
+		if(!empty($ligne)){
+				$_SESSION["id"] = $ligne["idUtilisateur"];
+				$_SESSION["nom"] = $ligne["nom"];
+				$_SESSION["prenom"] = $ligne["prenom"];
+				$_SESSION["Statu"] = $ligne["idStatu"];
+				echo "<meta http-equiv='refresh' content='1; URL=../Connexe/benevole.php'>";
+		}
+		else{
+				echo "<meta http-equiv='refresh' content='1; URL=../Connexe/connexion.php?msg=Vérifiez les identifiants'>";
+		}
 	}
 	else{
 		echo "<meta http-equiv='refresh' content='1; URL=../Connexe/connexion.php?msg=Merci de ne pas rentrer sur le site de manière illicite !'>";
 	}
-	$rep ->closeCursor();
+	$ligne ->closeCursor();
 	?>
-
 </body>
 </html>
