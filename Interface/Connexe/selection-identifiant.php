4214a3479885d31b8d1d7c3c25cc6ea88b01cf31
Amelia ANDRAUD <andraud.amelia@hotmail.com>
2020-04-12 22:12:41 +0200

    création du style pour selection-identifiant

--- Interface/Connexe/selection-identifiant.php
+++ Interface/Connexe/selection-identifiant.php
@@ -3,16 +3,18 @@
     <head>
     <title>Modifier un chien</title>
     <link rel="stylesheet" href="../Style/style.css" type="text/css" />
-    <link rel="stylesheet" href="../Style/ajout-chien.css" type="text/css" />
+    <link rel="stylesheet" href="../Style/selection-identifiant.css" type="text/css" />
     </head>
     <img id="logo" src="../Image/spaLogo.png" >
     <body>
+                   <img id="img" src="chienquibouge.gif">
+
         
         <h1>Modifier un chien </h1>
-        <br>
         <h2>Veuillez choisir un chien pour pouvoir le modifier :</h2>
         
     <form  method="get" action="modifier-chien.php" autocomplete="off">
+        <div id="div">
            <select class="fo" name="identifiant" id="identifiant">
             <option>Choisir un chien</option>
                     <?php
@@ -26,7 +28,9 @@
                 </select>
                 <br>
                 <br>
-    <input type="submit" name="valider" value="Valider">
-
+    <input style="margin-left: 75px;" type="submit" name="valider" value="Valider">
+</div>
 
 </form>
+</body>
+</html>
