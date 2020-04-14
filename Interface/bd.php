f487cd510b2d2e91f28e4d6b2ba27ebe3af2aaf3
Lucas Fioletti-Nieder <lulu57580@gmail.com>
2020-04-14 13:28:12 +0200

    ajout de deux fonctions qui permettent de modifier l'état de la session à ajouter dans toutes les pages sécurisées

--- Interface/bd.php
+++ Interface/bd.php
@@ -1,6 +1,16 @@
 <?php
 	function getBD(){
-		$bdd= new PDO('mysql:host=localhost;dbname=projetkitie;charset=utf8', 'root', '');
+		$bdd = new PDO('mysql:host=localhost;dbname=kitie-projet;port=3306','root','root');
 		return $bdd;
+	}
+	function sessionEmploye(){
+		if(!isset($_SESSION["Statut"]) or $_SESSION["Statut"]!=1 ){
+			echo "<meta http-equiv='refresh' content='0.000001; URL=connexion.php?msg=Merci, de vous connectez !'>";
+		}
+	}
+	function sessionActive(){
+		if(!isset($_SESSION["Statut"])){
+			echo "<meta http-equiv='refresh' content='0.000001; URL=connexion.php?msg=Merci, de vous connectez !'>";
+		}
 	}
 ?>
